84af0aaaec323658f325bde37ab0c55cf152ed3d
Ahassnat <alhassnatabdallah@gmail.com>
2021-10-10 13:14:51 +0300

    Add project files.

---
+++ AdvanceCSharpTopics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdvanceCSharpTopics
+{
+    class Program
+    {
+        // First Delegiate without any Parameters 
+        public delegate void FirstDelegateWithoutParameters();
+        static void Main(string[] args)
+        {
+            FirstDelegateWithoutParameters first = new FirstDelegateWithoutParameters(FirstFunctionWithTheSameDataTypeWithDelegate);
+            first.Invoke();
+            first();
+        }
+        public static void FirstFunctionWithTheSameDataTypeWithDelegate()
+        {
+            Console.WriteLine("Hello 1st Delegate Printing Hello");
+            Console.WriteLine("Hello");
+        }
+    }
+}
